5809e807dd3e5d1bb1f803c011c25e279cb8a5ce
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-08-03 12:46:46 +0200

    Inital commit

---
+++ src/main/java/no/imr/nmdapi/generic/response/v1/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/nmdapi/responses/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/nmdapi/responses/v1", prefix = "")
+})
+package no.imr.nmdapi.generic.response.v1;
